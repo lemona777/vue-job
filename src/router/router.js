58ff6dd97d998b4bd0ea491c563d55eeaea48f1a
BananaS2 <30709608+lemona777@users.noreply.github.com>
2025-02-21 20:27:36 +0900

    Add files via upload

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '../view/Login.vue'
 import SignUp from '../view/SignUp.vue'
 import JobPost from '../view/JobPost.vue'
@@ -39,7 +39,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 })
 
